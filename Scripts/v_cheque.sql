bc18040bf1494044270ca190d6f4d7d51f3d8b4a
Evgeny Rudenko <rudenkoevgeny@gmail.com>
2017-10-24 10:48:01 +1000

    Update v_cheque.sql

--- Scripts/v_cheque.sql
+++ Scripts/v_cheque.sql
@@ -1,9 +1,9 @@
--- прогнать скрипт по базе , чтобы работала выгрузка движений
+-- РїСЂРѕРіРЅР°С‚СЊ СЃРєСЂРёРїС‚ РїРѕ Р±Р°Р·Рµ , С‡С‚РѕР±С‹ СЂР°Р±РѕС‚Р°Р»Р° РІС‹РіСЂСѓР·РєР° РґРІРёР¶РµРЅРёР№
 
 CREATE VIEW [dbo].[v_cheque_pay_type]
 AS
 SELECT        dbo.LOT_MOVEMENT.ID_LOT_MOVEMENT, dbo.CHEQUE.PAY_TYPE_NAME, 
-                         CASE WHEN dbo.cheque.pay_type_name = 'Оплата наличными' THEN 21 WHEN dbo.CHEQUE.PAY_TYPE_NAME = 'Оплата платежной картой' THEN 22 END AS TableName
+                         CASE WHEN dbo.cheque.pay_type_name = 'РћРїР»Р°С‚Р° РЅР°Р»РёС‡РЅС‹РјРё' THEN 21 WHEN dbo.CHEQUE.PAY_TYPE_NAME = 'РћРїР»Р°С‚Р° РїР»Р°С‚РµР¶РЅРѕР№ РєР°СЂС‚РѕР№' THEN 22 END AS TableName
 FROM            dbo.CHEQUE_ITEM LEFT OUTER JOIN
                          dbo.CHEQUE ON dbo.CHEQUE_ITEM.ID_CHEQUE_GLOBAL = dbo.CHEQUE.ID_CHEQUE_GLOBAL RIGHT OUTER JOIN
                          dbo.LOT_MOVEMENT ON dbo.CHEQUE_ITEM.ID_CHEQUE_ITEM_GLOBAL = dbo.LOT_MOVEMENT.ID_DOCUMENT_ITEM
